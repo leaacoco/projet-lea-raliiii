d9ec6fe6fdb0dd5f2dc9cc17f08cf6bd1e24fb24
Rali EL KOHEN <ralielkohen@MacBook-Air-de-Rali.local>
2020-11-15 21:46:11 +0100

    ajout cordonnées initiale

--- Animal.java
+++ Animal.java
@@ -1,8 +1,8 @@
 
 public class Animal {
 	private String nom;
-	private int abs;
-	private int ord;
+	private int abs=5;
+	private int ord=10;
 	public void seDeplacer(int x, int y) {
 		abs+=x;
 		ord+=y;
